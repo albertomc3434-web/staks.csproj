a896f11e95b8696ce2ba8732ed617b624396a88a
ALBERTO MAYA CESAR <albertomc3434@gmail.com>
2026-05-21 07:51:23 -0600

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,27 @@
+// program.cs  –  Clase base (clase padre)
+
+class Animal
+{
+    // Atributos (propiedades)
+    public string Nombre;
+    public int Edad;
+
+    // Constructor
+    public Animal(string nombre, int edad)
+    {
+        Nombre = nombre;
+        Edad   = edad;
+    }
+
+    // Método
+    public void HacerSonido()
+    {
+        Console.WriteLine(Nombre + " hace un sonido.");
+    }
+
+    // Método para mostrar info
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Nombre: " + Nombre + ", Edad: " + Edad);
+    }
+}
